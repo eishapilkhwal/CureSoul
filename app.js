6b087ef2f5d92400db7fc4b5ac3bc46a4b320d50
AnkitaGupta2002 <115339717+AnkitaGupta2002@users.noreply.github.com>
2023-01-14 20:15:31 +0530

    Update app.js
    Adding js of FAQ to app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const buttons = document.querySelectorAll("button");
 
+//JS of navbar
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
@@ -23,3 +25,16 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+//JS of FAQ
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+    
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
